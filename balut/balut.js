c9bc6254aa994ca367753bde3882a7a4e090635c
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-01-20 15:25:18 -0600

    working on diceroll logic

--- balut/balut.js
+++ balut/balut.js
@@ -1,10 +1,53 @@
 "use strict";
+$(document).ready(function (){
+    function rng() {
+        let num = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        return num;
+    }
+
+    let roll = $("#roll");
+
+    let diceArr = [$("#d1.fas"),$("#d2.fas"), $("#d3.fas"),$("#d4.fas"),$("#d5.fas"),$("#d6.fas")]
 
 
+function diceRoller(die,num) {
+    console.log(num);
+    switch (num) {
+        case 1:
+            die.addClass("fa-dice-one");
+            ;
 
+            break;
+        case 2:
+            die.addClass("fa-dice-two");
 
-let num = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-console.log(num);
+            break;
+        case 3:
+            die.addClass("fa-dice-three");
+
+            break;
+        case 4:
+            die.addClass("fa-dice-four");
+
+            break;
+        case 5:
+            die.addClass("fa-dice-five")
+            break;
+        case 6:
+            die.addClass("fa-dice-six");
+            break;
+        default:
+            break;
+    }
+}
+ roll.click(function (){
+     for(let i =0; i<diceArr.length; i++){
+         diceRoller(diceArr[i],i);
+         diceRoller(diceArr[i],rng());
+     }
+ });
+});
+
 
 
 
