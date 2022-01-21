d4773ee2ecd3fc5b3f1e9c1c015b4b4bd9aab862
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-01-21 16:50:18 -0600

    worked on logic for dice roll

--- balut/balut.js
+++ balut/balut.js
@@ -1,4 +1,20 @@
 "use strict";
+//<div class="col-2">
+//                     <h1>
+//                         <i id="d1" class="fas white dice"></i>
+//                     </h1>
+//                 </div>
+
+function renderDice() {
+    var html = '<div class="col-2>';
+    html += '<h1>';
+    html += '<i class = "die"></i>';
+    html += '</h1>';
+    html += '</div>';
+    return html;
+}
+
+
 $(document).ready(function (){
     function rng() {
         let num = Math.floor(Math.random() * (6 - 1 + 1) + 1);
@@ -7,45 +23,59 @@ $(document).ready(function (){
 
     let roll = $("#roll");
 
-    let diceArr = [$("#d1.fas"),$("#d2.fas"), $("#d3.fas"),$("#d4.fas"),$("#d5.fas"),$("#d6.fas")]
+    let diceArr = [];
 
 
 function diceRoller(die,num) {
-    console.log(num);
-    switch (num) {
-        case 1:
-            die.addClass("fa-dice-one");
-            ;
+    let icon = $(".die");
+    if (icon.has(".die")) {
+        switch (num) {
+            case 1:
+                icon.addClass("fas white dice fa-dice-one");
+                ;
 
-            break;
-        case 2:
-            die.addClass("fa-dice-two");
+                break;
+            case 2:
+                icon.addClass("fas white dice fa-dice-two");
 
-            break;
-        case 3:
-            die.addClass("fa-dice-three");
+                break;
+            case 3:
+                icon.addClass("fas white dice fa-dice-three");
 
-            break;
-        case 4:
-            die.addClass("fa-dice-four");
+                break;
+            case 4:
+                icon.addClass("fas white dice fa-dice-four");
 
-            break;
-        case 5:
-            die.addClass("fa-dice-five")
-            break;
-        case 6:
-            die.addClass("fa-dice-six");
-            break;
-        default:
-            break;
+                break;
+            case 5:
+                icon.addClass("fas white dice fa-dice-five")
+                break;
+            case 6:
+                icon.addClass("fas white dice fa-dice-six");
+                break;
+            default:
+                break;
+        }
     }
+    console.log(num,die);
+    diceDiv.append(die);
 }
+
+
+
  roll.click(function (){
-     for(let i =0; i<diceArr.length; i++){
-         diceRoller(diceArr[i],i);
-         diceRoller(diceArr[i],rng());
+     for(let i =0; i<6; i++){
+
+      diceRoller(renderDice(),i);
      }
  });
+
+let diceDiv = $("#dice");
+diceArr.forEach(function(elem) {
+    console.log(elem);
+})
+// diceDiv.innerHTML = ;
+
 });
 
 
